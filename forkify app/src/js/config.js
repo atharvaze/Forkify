55b721fe579ac3b07731bf2b774698b26e3a3ab0
Atharva Waze <46229364+atharvaze@users.noreply.github.com>
2019-09-12 23:23:48 +0530

    Add files via upload

---
+++ forkify app/src/js/config.js
@@ -0,0 +1,4 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com';
+export const key= '13a31f8b2da5ccb2feca00a07fe361a5';
+// skey 13a31f8b2da5ccb2feca00a07fe361a5
+// akey 1b784922eeb1e519b688673f39fce840
